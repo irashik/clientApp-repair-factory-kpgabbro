bcfec1fc6d2a02b5af7ea0fb9f2b37a690deb405
Dmitrii Irashin <irashin.dim@gmail.com>
2021-07-18 08:50:44 +0300

    authcomponent is function

--- src/authpage.js
+++ src/authpage.js
@@ -7,7 +7,6 @@
 */
 
 import React, { useState } from "react";
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -18,104 +17,24 @@ import Form from 'react-bootstrap/Form';
 import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
 
 
+function AuthComponent() {
+  
+      const [email, setEmail] = useState("");
+      const [password, setPassword] = useState("");
+
+      function handeEmailChange(event) {
+        setEmail(event.target.value);
+      }
+
+      React.FormEvent
 
 
 
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('loginform: email: ' + email + '  password: ' + password);
+      }
 
-
-let userEmail = null;
-let userPassword = null;
-
-
-class AuthComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    };
-
-   // this.handleChange = this.handleChange1.bind(this);
-    this.handleChange = this.handleChange2.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  };
-
-
-
-  // handleChange1(event) {
-  //   console.log(event.target.value);
-
-  //   const target = event.target;
-
-  //   const value = target.type === 'email' ? target.email : target.password;
-
-
-  //   const name = target.name;
-
-  //   this.setState({[name]: value});
-    
-
-  // }
-
-  handleChange2(event) {
-    console.log(event.target.value);
-    this.setState({password: event.target.value});
-  }
-
-
-  // handleClick = buttonName => {
-  //   this.props.clickHandler(buttonName);
-  // };
-
-
-
-
- handleSubmit(event) {
-      event.preventDefault();
-      alert('отправленные данные' + this.state.email + "&& " + this.state.password);
-
-  }
-
-  // componentDidMount() {
-  //   const data = {
-  //     email: userEmail,
-  //     password: userPassword
-  //   }
-
-
-  //   const options = {
-  //     method: 'POST',
-  //     mode: 'cors',
-  //     cache: 'no-cache',
-  //     credentialls: 'same-origin',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     redirect: 'follow',
-  //     body: JSON.stringify(data)
-  //   };
-
-  //   fetch("http://localhost:3000/auth/login", options)
-  //     .then(res => res.json())
-  //     .then(
-  //       (result) => {
-  //       this.setState({
-  //         isLoaded: true,
-  //         items: result.items
-  //       });
-  //     },
-  //     (error) => {
-  //       this.setState({
-  //         isLoaded: true,
-  //         error
-  //       });
-  //     }
-  //     )
-    
-  // }
-
-
-    render() {
       return (
   
 
@@ -126,17 +45,18 @@ class AuthComponent extends React.Component {
           </Row>
          
           <Row>
-            <Form onSubmit={this.handleSubmit}>
-              {/* <Form.Group controlId="formBasicEmail">
+            <Form onSubmit={handleSubmit}>
+
+              <Form.Group controlId="formBasicEmail">
                   <Form.Control type="email" placeholder="Enter email" 
-                  value={this.state.email}
-                  onChange = {this.handleChange1}  />
-              </Form.Group> */}
+                  value = {email}
+                  onChange = {(e) => setEmail(e.target.value)}  />
+              </Form.Group>
 
               <Form.Group controlId="formBasicPassword">
                 <Form.Control type="password" placeholder="Password"
-                    value={this.state.password}
-                    onChange = {this.handleChange2} />
+                    value = {password}
+                    onChange = {(e) =>setPassword(e.target.value)} />
               </Form.Group>
           
               <Button variant="primary" type="submit" >
@@ -147,7 +67,7 @@ class AuthComponent extends React.Component {
         </Container>
 
       );
-    };
+    
 
 
 };
